f9188a60b6d8ae207568ec052583bc78c6f61ea2
mattloose <matt.loose@nottingham.ac.uk>
2021-03-12 20:19:48 +0000

    Update cli.py
    Remove list requirement.

--- aln2type/cli.py
+++ aln2type/cli.py
@@ -10,7 +10,7 @@ def parse_args():
     parser.add_argument("summary_csv_outfile", help="Output summary CSV file")
     parser.add_argument("ref_name", help="Name of reference sequence in MSA")
     parser.add_argument("msa", help="Path to MSA")
-    parser.add_argument("typing_yaml", nargs='+', help="Path to Variant definition YAML files")
+    parser.add_argument("typing_yaml", help="Path to Variant definition YAML files")
     args = parser.parse_args()
 
     return args
